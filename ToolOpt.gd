29edfae1a8e8df551aca59b768fd86b5e2fb2502
Mathias Plans <mathias.plans@estcube.eu>
2023-04-22 08:50:24 +0300

    Make selected tool/mode highlighted

--- ToolOpt.gd
+++ ToolOpt.gd
@@ -13,6 +13,14 @@ var mode = Mode.NONE
 	Mode.MULTI_CUT: $MultiCutOpt
 }
 
+@onready var mode_to_button = {
+	Mode.FACE_CUT: %ToolInfo/FaceCut,
+	Mode.TRI_POINT_CUT: %ToolInfo/TriPointCut,
+	Mode.TEST: %RuleOpt/TestGrammar,
+	Mode.PRISM_CUT: %ToolInfo/PrismCut,
+	Mode.MULTI_CUT: %ToolInfo/MultiCut
+}
+
 func _ready():
 	%ToolInfo/FaceCut.pressed.connect(_on_facecut_press)
 	%ToolInfo/TriPointCut.pressed.connect(_on_tripointcut_press)
@@ -48,11 +56,17 @@ func set_mode(_mode):
 	if self.mode != Mode.NONE:
 		var child = self.mode_to_child[self.mode]
 		child.visible = false
+		
+		var button = self.mode_to_button[self.mode]
+		button.button_pressed = false
 	
 	# Enable new
 	if _mode != Mode.NONE:
 		var child = self.mode_to_child[_mode]
 		child.visible = true
+		
+		var button = self.mode_to_button[_mode]
+		button.button_pressed = true
 	
 	var old_mode = self.mode
 	self.mode = _mode
